4eae5b80f4945c595ea3797045460c6f8687449c
chanhle <giaosudau@gmail.com>
2023-08-16 17:37:27 +0700

    add test for currency converting

--- tests/test_models.py
+++ tests/test_models.py
@@ -6,8 +6,8 @@ from models import parse_currency
 class TestModel(unittest.TestCase):
 
     def test_parse_currency(self):
-        self.assertEquals(0, parse_currency('€0k'))
-        self.assertEquals(10000, parse_currency('€10k'))
-        self.assertEquals(33700000, parse_currency('€33.70m'))
-        self.assertEquals(1250000000, parse_currency('€1.25bn'))
-        self.assertEquals(None, parse_currency(None))
+        self.assertEqual(0, parse_currency('€0k'))
+        self.assertEqual(10000, parse_currency('€10k'))
+        self.assertEqual(33700000, parse_currency('€33.70m'))
+        self.assertEqual(1250000000, parse_currency('€1.25bn'))
+        self.assertEqual(None, parse_currency(None))
